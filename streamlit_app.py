c6c4d1c7e2b7e66fa13213bf0d174973dbfbdd2a
NickFLPP <nicole.wishart@live.com>
2024-07-15 16:13:32 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.functions import col, when_matched
-import requests
+from snowflake.snowpark.functions import col
+#, when_matched
+#import requests
 
 
 # Write directly to the app
